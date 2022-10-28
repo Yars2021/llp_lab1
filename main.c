85201b8998778f7cb47fb527fdfc1aed5a4016b8
Yars2021 <yaroslav.sukhovey@gmail.com>
2022-10-28 16:05:09 +0300

    Added partial table parsers

--- main.c
+++ main.c
@@ -6,22 +6,24 @@
 {"TABLE_NAME":"TestTable","TABLE_SIZE":"3","TABLE_SCHEMA":{"KEY_COL_I":"2","NUM_OF_FIELDS":"3","FIELDS":[{"F_NAME":"string","F_TYPE":"S"},{"F_NAME":"randomCol","F_TYPE":"F"},{"F_NAME":"idCol","F_TYPE":"I"}]},"TABLE_DATA":[{"RECORD":["VAL-1","VAL-2","VAL_ID"]},{"RECORD":["VAL-5","VAL-452","VAL_ID"]},{"RECORD":["VAL-5435","VAL-452","VAL_ID"]}]}
  */
 
+// Parsing the whole table VS header + 1 record
 int main() {
+    char *line = "{\"TABLE_NAME\":\"TestTable\",\"TABLE_SIZE\":\"3\",\"TABLE_SCHEMA\":{\"KEY_COL_I\":\"2\",\"NUM_OF_FIELDS\":\"3\",\"FIELDS\":[{\"F_NAME\":\"string\",\"F_TYPE\":\"S\"},{\"F_NAME\":\"randomCol\",\"F_TYPE\":\"F\"},{\"F_NAME\":\"idCol\",\"F_TYPE\":\"I\"}]},\"TABLE_DATA\":[{\"RECORD\":[\"VAL-5435\",\"VAL-452\",\"VAL_ID\"]},{\"RECORD\":[\"VAL-5\",\"VAL-452\",\"VAL_ID\"]},{\"RECORD\":[\"VAL-1\",\"VAL-2\",\"VAL_ID\"]}]}";
+    size_t records = 0;
 
-   // Table *table = parseTableJSON("{\"TABLE_NAME\":\"TestTable\",\"TABLE_SIZE\":\"3\",\"TABLE_SCHEMA\":{\"KEY_COL_I\":\"2\",\"NUM_OF_FIELDS\":\"3\",\"FIELDS\":[{\"F_NAME\":\"string\",\"F_TYPE\":\"S\"},{\"F_NAME\":\"randomCol\",\"F_TYPE\":\"F\"},{\"F_NAME\":\"idCol\",\"F_TYPE\":\"I\"}]},\"TABLE_DATA\":[{\"RECORD\":[\"VAL-5435\",\"VAL-452\",\"VAL_ID\"]},{\"RECORD\":[\"VAL-5\",\"VAL-452\",\"VAL_ID\"]},{\"RECORD\":[\"VAL-1\",\"VAL-2\",\"VAL_ID\"]}]}");
-   // printf("%s", transformTableToJSON(table));
+    Table *table = parseTableJSON(line, 0);
+    printf("%s\n", transformTableToJSON(table));
 
-   // destroyTable(table);
+    destroyTable(table);
 
-    DBFileHeader *dbFileHeader = createDBFileHeader("/home/yars/CLionProjects/llp_lab1_c/.database");
+    table = parseTableHeaderJSON(line, 0, &records);
+    table->length = 0;
 
-    printf("%s\n", dbFileHeader->target_file_path);
-    printf("%s\n", dbFileHeader->db_name);
-    printf("%d\n", dbFileHeader->number_of_nodes);
+    insertTableRecord(table, parseTableRecordJSON(line, records, table->tableSchema, &records));
 
-    persistDatabase(dbFileHeader);
+    printf("%s\n", transformTableToJSON(table));
 
-    free(dbFileHeader);
+    destroyTable(table);
 
     return 0;
 }
